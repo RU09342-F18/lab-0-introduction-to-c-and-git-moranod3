15a55e0df44e1a97c7f455e3e341d8e3f9213f73
moranod3 <35497808+moranod3@users.noreply.github.com>
2018-09-10 14:48:45 -0400

    Create math.c
    Implementation of math.c based off the math.h header file.

---
+++ Assignment/math.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "math.h"
+
+int math(U16 num1, int num2, char Operator){
+int out;
+int choice;
+switch (Operator)
+{
+case '+': 
+	out = num1+num2;
+	break;
+case '-':
+	out = num1-num2;
+	break;
+case '*':
+	out = num1*num2;
+	break;
+case '/':
+	out = num1/num2;
+	break;
+case '%':
+	out = num1%num2;
+	break;
+case '<':
+	out = num1 << num2;
+	break;
+case '>':
+	out = num1 >> num2;
+	break;
+case '&':
+	out = num1&num2;
+	break;
+case '|':
+	out = num1|num2;
+case '^':
+	out = num1^num2;
+	break;
+case '~':
+	scanf("%d", choice);
+	if (choice == 1)
+		out = ~num1;
+	if (choice == 2)
+		out = ~num2;
+	break;
+default:
+	printf("%c" , "ERROR");
+	break;
+}
+return out;
+}
