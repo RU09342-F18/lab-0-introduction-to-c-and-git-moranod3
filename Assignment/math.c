deac7a43a84e103accd56ec9fcd6389b110d136e
moranod3 <35497808+moranod3@users.noreply.github.com>
2018-09-10 16:25:02 -0400

    Update math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -1,10 +1,9 @@
-#include "math.h"
-#include <stdio.h>
+#include "math.h"  // Include of the header file
+#include <stdio.h> // Include the standard in out file for scanf and printf functions
 
 int math(U16 num1, int num2, char Operator){
-int out;
-int choice;
-switch (Operator)
+int out, choice; // out is used as the output of the function, choice is used when the inverse function is needed
+switch (Operator)  //Switch case which is used to determine what operation to use
 {
 case '+': 
 	out = num1+num2;
@@ -36,15 +35,20 @@ case '^':
 	out = num1^num2;
 	break;
 case '~':
-	scanf("%d", choice);
-	if (choice == 1)
+	scanf("%d", choice); // scanf to determine what number to inverse
+	if (choice == 1){
 		out = ~num1;
-	if (choice == 2)
+	}
+	if (choice == 2){
 		out = ~num2;
+	}
+	else{
+		printf("%s", "ERROR");
+	}
 	break;
 default:
-	printf("%c" , "ERROR");
+	printf("%s" , "ERROR");
 	break;
 }
-return out;
+return out; //Return statement, this is the output of calling the function
 }
